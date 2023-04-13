3fb3abd8df881d4d53396b48f799cd28dec2f45f
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-04-13 10:10:32 +0300

    start hw2

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+        style={styles.image}
+        source={require("./assets/images/PhotoBG.png")}
+      >
+        <View style={styles.form}>
+          <View>
+            <TextInput
+              style={styles.input}
+              textAlign={"center"}
+              placeholder={"Логін"}
+            />
+          </View>
+          <View>
+            <TextInput
+              style={styles.input}
+              textAlign={"center"}
+              placeholder={"Адреса електронної пошти"}
+            />
+          </View>
+          <View style={{ marginTop: 20 }}>
+            <TextInput
+              style={styles.input}
+              textAlign={"center"}
+              placeholder={"Пароль"}
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+      </ImageBackground>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +49,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  input: {
+    margin: 50,
+    width: 100,
+    borderColor: "red",
+    borderStyle: "solid",
+    borderWidth: 1,
+  },
+  form: {
+    height: "100%",
+    backgroundColor: "white",
+    marginTop: 263,
+    borderRadius: 25,
     alignItems: "center",
-    justifyContent: "center",
   },
 });
