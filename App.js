8ab208471c6a3b8f4bc0880a7243384a39f9f694
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-04-23 17:02:25 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,42 +1,90 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
   ImageBackground,
   TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 
 export default function App() {
+  const [isSecure, setIsSecure] = useState(false);
+  const [keyboard, setKeyboard] = useState(false);
+  const [state, setState] = useState({});
+  const omSecureClick = () => {
+    setIsSecure(!isSecure);
+  };
   return (
     <View style={styles.container}>
       <ImageBackground
         style={styles.image}
         source={require("./assets/images/PhotoBG.png")}
       >
-        <View style={styles.form}>
-          <Text style={styles.title}>Реєстрація</Text>
-          <TextInput
-            style={styles.input}
-            textAlign={"center"}
-            placeholder={"Логін"}
-          />
-
-          <TextInput
-            style={styles.input}
-            textAlign={"center"}
-            placeholder={"Адреса електронної пошти"}
-          />
-
-          <TextInput
-            style={styles.input}
-            textAlign={"center"}
-            placeholder={"Пароль"}
-            secureTextEntry={true}
-          />
-        </View>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+          style={{ width: "100%" }}
+        >
+          <View style={styles.form}>
+            <Text style={styles.title}>Реєстрація</Text>
+            <View style={styles.input}>
+              <TextInput
+                textAlign={"center"}
+                alignItems={"center"}
+                height={"100%"}
+                placeholder={"Логін"}
+                onFocus={() => setKeyboard(true)}
+                value={state.login}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, login: value }))
+                }
+              />
+            </View>
+            <View style={styles.input}>
+              <TextInput
+                textAlign={"center"}
+                alignItems={"center"}
+                height={"100%"}
+                onFocus={() => setKeyboard(true)}
+                placeholder={"Адреса електронної пошти"}
+                value={state.email}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, email: value }))
+                }
+              />
+            </View>
+            <View style={styles.input} position={"relative"}>
+              <TextInput
+                textAlign={"center"}
+                alignItems={"center"}
+                height={"100%"}
+                placeholder={"Пароль"}
+                onFocus={() => setKeyboard(true)}
+                secureTextEntry={isSecure}
+                value={state.password}
+                onChangeText={(value) =>
+                  setState((prevState) => ({ ...prevState, password: value }))
+                }
+              />
+              <TouchableOpacity
+                style={styles.secureBtn}
+                onPress={omSecureClick}
+              >
+                <Text style={styles.secureItem}>
+                  {isSecure ? "Показати" : "Скрити"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.submit}>
+              <Text style={styles.btnItem}>Зареєструватися</Text>
+            </TouchableOpacity>
+            <Text style={styles.maybeLogin}>Вже є аккаунт? Увійти</Text>
+          </View>
+        </KeyboardAvoidingView>
       </ImageBackground>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -49,11 +97,10 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    // resizeMode: "cover",
-    position: "absolute",
-    top: 0,
-    right: 0,
-    left: 0,
+    width: "100%",
+    height: "100%",
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
   },
   input: {
     marginTop: 16,
@@ -66,16 +113,45 @@ const styles = StyleSheet.create({
     backgroundColor: "#F6F6F6",
   },
   form: {
-    height: "100%",
-    backgroundColor: "white",
-    marginTop: 263,
-    borderRadius: 25,
+    display: "flex",
     alignItems: "center",
+    position: "relative",
+    width: "100%",
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
     paddingHorizontal: 16,
   },
   title: {
-    color: "black",
     marginTop: 92,
     fontSize: 30,
+  },
+  submit: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 43,
+    backgroundColor: "#FF6c00",
+    width: "100%",
+    height: 50,
+    borderRadius: 25,
+  },
+  btnItem: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  secureBtn: {
+    position: "absolute",
+    backgroundColor: "transparent",
+    right: 16,
+    top: 16,
+  },
+  secureItem: {
+    color: "#1B4371",
+  },
+  maybeLogin: {
+    marginTop: 16,
+    marginBottom: 78,
+    color: "#1B4371",
+    fontSize: 16,
   },
 });
